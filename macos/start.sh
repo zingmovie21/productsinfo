eb3effa6bbce654de83cb607d9a2e033780422d3
zingmovie21 <133083254+zingmovie21@users.noreply.github.com>
2024-10-27 15:07:01 +0530

    Create start.sh

---
+++ macos/start.sh
@@ -0,0 +1,25 @@
+# Downloads
+curl -s -o login.sh -L "https://raw.githubusercontent.com/JohnnyNetsec/github-vm/main/mac/login.sh"
+# Disable Spotlight indexing
+sudo mdutil -i off -a
+# Create new account
+sudo dscl . -create /Users/runneradmin
+sudo dscl . -create /Users/runneradmin UserShell /bin/bash
+sudo dscl . -create /Users/runneradmin RealName Runner_Admin
+sudo dscl . -create /Users/runneradmin UniqueID 1001
+sudo dscl . -create /Users/runneradmin PrimaryGroupID 80
+sudo dscl . -create /Users/runneradmin NFSHomeDirectory /Users/tcv
+sudo dscl . -passwd /Users/runneradmin P@ssw0rd!
+sudo createhomedir -c -u runneradmin > /dev/null
+sudo dscl . -append /Groups/admin GroupMembership runneradmin
+# Enable VNC
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
+echo runnerrdp | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
+# Start VNC/reset changes
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
+# Install Localtonet
+brew install --cask localtonet
+# Configure Localtonet and start it
+localtonet --auth-token "$1" rdp --local localhost:3389 &
